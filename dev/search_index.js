e8f27995ddbf546c71212b2ded4286ef0c7da9ed
Documenter.jl <documenter@juliadocs.github.io>
2024-09-24 11:51:09 +0000

    build based on 9d6657b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ApproximateRelations","category":"page"},{"location":"#ApproximateRelations","page":"Home","title":"ApproximateRelations","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ApproximateRelations.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ApproximateRelations]","category":"page"}]
+}
